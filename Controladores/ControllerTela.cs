57177a213a8847202dc3ecc682156ba5d0fd9f2e
Jonathan <jonathan@stardust.com.mx>
2021-01-13 23:41:31 -0600

    funcion get inventario en el controlador

--- Controladores/ControllerTela.cs
+++ Controladores/ControllerTela.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,10 @@ namespace Controladores
         ArrayList DATA_TELAS;
         Tela tela;
 
+        public DataTable getInventario()
+        {
+            return omt.getInventario();
+        }
         public void setDataTelas()
         {
             DATA_TELAS = new ArrayList();
